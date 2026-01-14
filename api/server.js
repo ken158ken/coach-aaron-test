589b24628009d38535e1e4df6ceba329adf7f920
Kenensh <ken150ken150@tmu.edu.tw>
2026-01-15 02:25:54 +0800

    fix: remove framework:null from vercel.json

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 /**
  * Vercel Serverless API Handler
  * 將所有請求轉發到 backend Express app
- * 
+ *
  * @module api/server
  */
 
-const app = require('../backend/index');
+const app = require("../backend/index");
 
 module.exports = app;
